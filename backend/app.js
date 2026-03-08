c5754ec56fa29043bc80e7f7e9eb2c8530ece401
srikar <vemisrikar@gmail.com>
2026-03-08 20:29:47 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+let mongoose=require("mongoose");
+let express=require("express");
+let app=express();
+app.listen(3000,()=>{
+    console.log("server started")
+})
+mongoose.connect(process.env.mongoose_url).then(()=>{
+    console.log("database connected");
+}).catch((err)=>{
+    console.log("err:",err)
+})
